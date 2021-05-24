149be3b5f70cc139116ab2754d7e64bfa7a3f3cb
vohoaidanh <danhvohoai2@gmail.com>
2021-05-24 16:23:58 +0700

    first commit

--- myfirstsite/demo/views.py
+++ myfirstsite/demo/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Chao Danh</h1>')
+    #return HttpResponse('<h1>Chao Danh</h1>')
+    return render(request,'demo/hello.html')
